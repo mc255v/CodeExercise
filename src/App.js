655a8aea5dfe34bc683e04159ae7b8a1f828eb8c
Matthew Cook <mc255v@gmail.com>
2019-11-11 17:50:56 +0900

    added media list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/App.css';
 import FilterBar from './components/FIlterBar';
+import MediaList from './components/MediaList';
 
 function App() {
   return (
     <div className="App">
       <FilterBar />
+      <MediaList />
     </div>
   );
 }
